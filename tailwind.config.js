59c5c5421cf78349d6f46552f0ae79f326f1074f
Manuchehrzoda <mansurimanuchehr@icloud.com>
2025-07-16 09:46:12 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      keyframes: {
+        run: {
+          '0%': { transform: 'rotate(0deg)' },
+          '50%': { transform: 'rotate(180deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        }
+      },
+      animation: {
+        simple: 'run 1s ease infinite'
+      },
+      colors: {
+        'red': '#ff0000',
+      }
+    },
+    screens: {
+      'mx': { max: '510px' }
+    }
+  },
+  plugins: [],
+}
